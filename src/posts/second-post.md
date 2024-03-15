c99b3e9a38549bae4429e52956ffe429e948d95c
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2024-03-15 12:28:12 -0300

    dark

--- src/posts/second-post.md
+++ src/posts/second-post.md
@@ -1,15 +1,25 @@
 ---
 title: Second
 description: Second post.
-date: '2024-3-14'
+date: '2024-3-16'
 categories:
   - sveltekit
   - svelte
 published: true
 ---
 
+<script>
+  import Counter from './counter.svelte'
+</script>
+
+## Table of Contents
+
 ## Svelte
 
 Media inside the **static** folder is served from `/`.
 
 ![Svelte](favicon.png)
+
+## Counter
+
+<Counter />
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,31 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-import { mdsvex } from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { getHighlighter } from 'shiki'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+    rehypePlugins: [rehypeSlug]
 }
 
 /** @type {import('@sveltejs/kit').Config} */
